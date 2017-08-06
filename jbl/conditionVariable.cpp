ae9b0896e15a08b921cd3d66e9dad5382aa5b4ef
Jeff Hutchinson <programmerjeff95@gmail.com>
2017-08-06 18:53:43 -0400

    finish updating license to MIT license, and implement workaround for condition variables in Window XP and below.

--- jbl/conditionVariable.cpp
+++ jbl/conditionVariable.cpp
@@ -1,45 +1,75 @@
 //-----------------------------------------------------------------------------
 // mutex.cpp
 //
-// Copyright(c) 2016, Jeff Hutchinson
-// All rights reserved.
+// Copyright (c) 2016-2017 Jeff Hutchinson
 //
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are met :
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
 //
-// * Redistributions of source code must retain the above copyright notice, this
-//   list of conditions and the following disclaimer.
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
 //
-// * Redistributions in binary form must reproduce the above copyright notice,
-//   this list of conditions and the following disclaimer in the documentation
-//   and / or other materials provided with the distribution.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 //
-// * Neither the name of jbl nor the names of its
-//   contributors may be used to endorse or promote products derived from
-//   this software without specific prior written permission.
-//
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-// DISCLAIMED.IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-// DAMAGES(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-// OR TORT(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// Note: The Win32 Condition Variable code for Windows NT below Vista
+// is based off of Douglas C. Schmidt and Irfan Pyarali.
+// The implementation is here: http://www.cs.wustl.edu/~schmidt/win32-cv-1.html
+// The copyright is here: http://www.cs.wustl.edu/~schmidt/ACE-copying.html
+// Note that this method is also used inside of glfw2 and TinyCppThread.
+// This is needed because Windows NT 5.X and lower do not implement native
+// support for condition variables.
 //-----------------------------------------------------------------------------
 
+#include <assert.h>
+#ifdef _WIN32
+#include <VersionHelpers.h>
+#endif
 #include "jbl/conditionVariable.h"
+
+bool ConditionVariable::sInitialized = false;
+#ifdef _WIN32
+	bool ConditionVariable::sIsVistaOrGreater = false;
+#endif
+
+void ConditionVariable::init()
+{
+#ifdef _WIN32
+	sIsVistaOrGreater = IsWindowsVistaOrGreater();
+#endif
+	
+	sInitialized = true;
+}
 
 ConditionVariable::ConditionVariable()
 {
+	assert(sInitialized);
+	
 #ifdef _WIN32
-#if WINVER >= 0x0600
-	InitializeConditionVariable(&mConditionVariable);
-#else
-#error "Please implement condition variable workaround on Windows XP / Server 2003"
-#endif
+	if (sIsVistaOrGreater)
+	{
+		InitializeConditionVariable(&mConditionVariable);
+	}
+	else
+	{
+		mLegacyConditionVariable.waitersCount = 0;
+		InitializeCriticalSection(&mLegacyConditionVariable.waitersCountLock);
+		
+		// Auto Reset Event
+		mLegacyConditionVariable.events[eSIGNAL] = CreateEvent(NULL, FALSE, FALSE, NULL);
+		
+		// Manual Reset Event
+		mLegacyConditionVariable.events[eBROADCAST] = CreateEvent(NULL, TRUE, FALSE, NULL);
+	}
 #else
 	pthread_cond_init(&mConditionVar, nullptr);
 #endif
@@ -47,14 +77,18 @@ ConditionVariable::ConditionVariable()
 
 ConditionVariable::~ConditionVariable()
 {
+	assert(sInitialized);
+	
 #ifdef _WIN32
-#if WINVER >= 0x0600
-	// There is no need to delete a condition variable object, as there is no
-	// dynamic memory being allocated.
-	// http://stackoverflow.com/a/28981408
-#else
-#error "Please implement condition variable workaround on Windows XP / Server 2003"
-#endif
+	// Vista condition variable has no cleanup. See MSDN for more information.
+	if (!sIsVistaOrGreater)
+	{
+		DeleteCriticalSection(&mLegacyConditionVariable.waitersCountLock);
+		
+		// Kill events
+		CloseHandle(mLegacyConditionVariable.events[eSIGNAL]);
+		CloseHandle(mLegacyConditionVariable.events[eBROADCAST]);
+	}
 #else
 	pthread_cond_destroy(&mConditionVar);
 #endif
@@ -62,12 +96,42 @@ ConditionVariable::~ConditionVariable()
 
 void ConditionVariable::wait(Mutex *mutex)
 {
+	assert(sInitialized);
+	
 #ifdef _WIN32
-#if WINVER >= 0x0600
-	SleepConditionVariableCS(&mConditionVariable, &mutex->mMutex, INFINITE);
-#else
-#error "Please implement condition variable workaround on Windows XP / Server 2003"
-#endif	
+	if (sIsVistaOrGreater)
+	{
+		SleepConditionVariableCS(&mConditionVariable, &mutex->mMutex, INFINITE);
+	}
+	else
+	{
+		EnterCriticalSection(&mLegacyConditionVariable.watiersCountLock);
+		{
+			mLegacyConditionVariable.waitersCount++;
+		}
+		LeaveCriticalSection(&mLegacyConditionVariable.watiersCountLock);
+		
+		mutex.unlock();
+		
+		int result = WaitForMultipleObjects(2, mLegacyConditionVariable.events, FALSE, INFINITE);
+
+		int lastWaiter = 0;
+		EnterCriticalSection(&mLegacyConditionVariable.watiersCountLock);
+		{
+			mLegacyConditionVariable.waitersCount--;
+			lastWaiter = result == WAIT_OBJECT_0 + eBROADCAST
+				&& mLegacyCondtionVariable.waitersCount == 0;
+		}
+		LeaveCriticalSection(&mLegacyConditionVariable.watiersCountLock);
+
+		if (lastWaiter)
+		{
+			// Last waiter to be notified, reset manual event.
+			ResetEvent(mLegacyConditionVariable.events[eBROADCAST]);
+		}
+		
+		mutex.lock();
+	}
 #else
 	pthread_cond_wait(&mConditionVar, &mutex->mMutex);
 #endif
@@ -75,12 +139,25 @@ void ConditionVariable::wait(Mutex *mutex)
 
 void ConditionVariable::signal()
 {
+	assert(sInitialized);
+	
 #ifdef _WIN32
-#if WINVER >= 0x0600
-	WakeConditionVariable(&mConditionVariable);
-#else
-#error "Please implement condition variable workaround on Windows XP / Server 2003"
-#endif
+	if (sIsVistaOrGreater)
+	{
+		WakeConditionVariable(&mConditionVariable);
+	}
+	else
+	{
+		EnterCriticalSection(&mLegacyConditionVariable.watiersCountLock);
+		int hasWaiters = mLegacyConditionVariable.waitersCount > 0;
+		LeaveCriticalSection(&mLegacyConditionVariable.watiersCountLock);
+		
+		if (hsaWaiters)
+		{
+			// Go ahead and signal
+			SetEvent(mLegacyConditionVariable.events[eSIGNAL]);
+		}
+	}
 #else
 	pthread_cond_signal(&mConditionVar);
 #endif
@@ -88,12 +165,25 @@ void ConditionVariable::signal()
 
 void ConditionVariable::signalAll()
 {
+	assert(sInitialized);
+	
 #ifdef _WIN32
-#if WINVER >= 0x0600
-	WakeAllConditionVariable(&mConditionVariable);
-#else
-#error "Please implement condition variable workaround on Windows XP / Server 2003"
-#endif	
+	if (sIsVistaOrGreater)
+	{
+		WakeAllConditionVariable(&mConditionVariable);
+	}
+	else
+	{
+		EnterCriticalSection(&mLegacyConditionVariable.watiersCountLock);
+		int hasWaiters = mLegacyConditionVariable.waitersCount > 0;
+		LeaveCriticalSection(&mLegacyConditionVariable.watiersCountLock);
+		
+		if (hsaWaiters)
+		{
+			// Go ahead and broadcast
+			SetEvent(mLegacyConditionVariable.events[eBROADCAST]);
+		}
+	}
 #else
 	pthread_cond_broadcast(&mConditionVar);
 #endif
